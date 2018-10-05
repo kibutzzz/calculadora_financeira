465d9ec7a8d4c59c5d11c76364180b9342fb71ed
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2018-10-04 21:34:37 -0300

    implementado calculo de juros composto para todos os campos

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def calcular_simples(montante, principal, taxa, periodos_decorridos):
     if opcao is 2:
         input_taxa_de_juros.insert(0, (calc.calcular_juros_simples(
             valor_inicial=float(principal),
-            juros=float(montante )- float(principal),
+            juros=float(montante) - float(principal),
             periodos=float(periodos_decorridos)
         )) * 100)
 
@@ -68,11 +68,62 @@ def calcular_simples(montante, principal, taxa, periodos_decorridos):
         input_periodos.insert(0, (calc.calcular_juros_simples(
             valor_inicial=float(principal),
             juros=float(montante) - float(principal),
+            taxa_de_juros=float(taxa) / 100
+        )))
+
+
+def calcular_composto(montante, principal, taxa, periodos_decorridos):
+    campos_vazios = 0
+    opcao = 0
+    if montante == "":
+        montante = None
+        campos_vazios += 1
+        opcao = 0
+    if principal == "":
+        principal = None
+        campos_vazios += 1
+        opcao = 1
+    if taxa == "":
+        taxa = None
+        campos_vazios += 1
+        opcao = 2
+    if periodos_decorridos == "":
+        periodos_decorridos = None
+        campos_vazios += 1
+        opcao = 3
+
+    if campos_vazios is not 1:
+        print("numero de valores repassados não está correto")
+        return
+
+    if opcao is 0:
+        input_montante.insert(0, (calc.calcular_juros_compostos(
+            valor_inicial=float(principal),
+            taxa_de_juros=float(taxa) / 100,
+            periodos=float(periodos_decorridos)
+        )))
+
+    if opcao is 1:
+        input_valor_inicial.insert(0, (calc.calcular_juros_compostos(
+            montante=float(montante),
+            taxa_de_juros=float(taxa) / 100,
+            periodos=float(periodos_decorridos)
+        )))
+
+    if opcao is 2:
+        input_taxa_de_juros.insert(0, (calc.calcular_juros_compostos(
+            valor_inicial=float(principal),
+            montante=float(montante),
+            periodos=float(periodos_decorridos)
+        )) * 100)
+
+    if opcao is 3:
+        input_periodos.insert(0, (calc.calcular_juros_compostos(
+            valor_inicial=float(principal),
+            montante=float(montante),
             taxa_de_juros=float(taxa)/100
         )))
 
-def calcular_composto():
-    pass
 
 tela.button(15, 175, "Calcular juros simples", command=lambda: (
     calcular_simples(
@@ -84,7 +135,12 @@ tela.button(15, 175, "Calcular juros simples", command=lambda: (
 ))
 
 tela.button(15, 200, "Calcular juros composto", command=lambda: (
-    calcular_composto()
+    calcular_composto(
+        input_montante.get(),
+        input_valor_inicial.get(),
+        input_taxa_de_juros.get(),
+        input_periodos.get()
+    )
 ))
 
 tela.button(15, 225, "Limpar", command=lambda: (
